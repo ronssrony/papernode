f27ad0ac297025896b45a4f20da170872d18515c
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2024-05-21 23:20:46 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
-const express = require("express") ;
+
+const express = require('express')
 const app = express();
-const path = require('path') ;
-const mysql = require("mysql") ;
-const dotenv = require('dotenv') ;
-const hbs = require('hbs');
-dotenv.config({path:'./.env'}) ;
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const session = require('express-session');
+const path = require("path");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const hbs = require("hbs");
+dotenv.config({ path: "./.env" });
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const fileUpload = require("express-fileupload") ;
+const bodyParser = require("body-parser") ;
+const stripe = require("stripe") ;
+const { Client } = require('pg');
+
+const client = new Client({
+  host: 'dpg-cp6d6vo21fec738gc630-a',
+  user: 'ptraylogin_user', // replace with your PostgreSQL username
+  password: 'Gq9PlY1OUSplKOKJUjOrWWsOYnqXmrqv', // replace with your PostgreSQL password
+  database: 'ptraylogin',
+  port: 5432, // default port for PostgreSQL
+
+  ssl: {
+    rejectUnauthorized: false,  // This is important for Render connections
+  },
+});
+
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL database'))
+  .catch(err => console.error('Connection error', err.stack));
 
 
-const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'ptray-login'
-    
-}); 
 
-    db.connect((error) => {
-        if(error){
-            console.log(error)
-        } 
-        else{
-            console.log("MYSQL connected")
-        }
-    });
-   
-   
- const publicDirectory = path.join(__dirname,'./public')   
- 
- app.use(express.static(publicDirectory)) ;
- app.use(express.urlencoded({extended:false})) ;
- app.use(express.json()) ; 
- app.use(session({
-    secret: 'ronss1815rony1425////',
+const publicDirectory = path.join(__dirname, "./public");
+
+app.use(express.static(publicDirectory));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json()) ;
+app.use(fileUpload()) ;
+app.use(
+  session({
+    secret: "ronss1815rony1425////",
     resave: false,
-    saveUninitialized: true
-  }));
- 
- app.set('view engine', 'hbs'); 
-app.use('/', require('./routes/pages')); 
-app.use('/auth', require('./routes/auth')); 
-
-app.listen(5000 , () => {
-    console.log("server is started on port 5000")
-}) ; 
+    saveUninitialized: true,
+  })
+);
 
 
+
+app.set("view engine", "hbs");
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
+
+app.listen(5000, () => {
+  console.log("server is started on port 5000");
+});
+
+let stripeGateway = stripe(process.env.stripe_api) ;
